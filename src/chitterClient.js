5845548cecd9c52fa66040a6807f976ecd15131a
narae <narae1096@gmail.com>
2023-04-11 11:12:03 +0100

    fetch all peeps api

--- src/chitterClient.js
+++ src/chitterClient.js
@@ -1,3 +1,17 @@
-class chitterClient{
-    
+class ChitterClient{
+  #BASE_URL = "https://chitter-backend-api-v2.herokuapp.com/";
+  
+  loadPeeps(cb) {
+    fetch(this.#BASE_URL + "peeps")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Peeps are loaded');
+        cb(data)
+      })
+    .catch(error => {
+      console.log("Failed!", error);
+    })
+  }
 }
+
+module.exports = ChitterClient;
--- tests/chitterClient.test.js
+++ tests/chitterClient.test.js
@@ -6,4 +6,21 @@ describe('client class', () => {
   beforeEach(() => {
     fetch.resetMocks();   
   });
+
+  it('calls fetch and loads all peeps', (done) => {
+    const client = new ChitterClient();
+    fetch.mockResponseOnce(JSON.stringify({
+      "id": 3,
+      "body": "my first peep :)"
+    }));
+    
+    client.loadPeeps((returnedData) => {
+      expect(returnedData).toEqual({ 
+        "id": 3, 
+        "body": "my first peep :)" 
+      });
+
+      done();
+    });
+  });
 })
